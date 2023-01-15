eadb651125f3f121782729d383d6b72d2ada40bd
behnam1369 <behnam.aghaali@yahoo.com>
2023-01-15 21:26:54 +0330

    Update application config

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module HomestretchApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoloader = :classic
   end
 end
