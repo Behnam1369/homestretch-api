6332852e467a3e70d16099a1bd769656cb7d9efa
behnam1369 <behnam.aghaali@yahoo.com>
2023-01-22 00:05:56 +0330

    Add profile controller and actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def show_profile
+    render json: current_user
+  end
+
+  def save_profile
+    if current_user.update(profile_params)
+      render json: { message: 'Success'}
+    else
+      render json: { message: 'Error', errors: current_user.errors }
+    end
+  end
+
+  def upload_avatar
+    path = Rails.root.join('public', 'files',"#{current_user.id}.png")
+    File.binwrite(path, params[:file].read)
+    render json: { message: 'Success'}
+  end
+
+  def profile_params
+    params.require(:user).permit(
+      :fname, :lname, :phone, :birth_date, :veteran_status, :home_buying_status, :race, :address1, :address2, :city, :zipcode, 
+      :employment_status, :income, :debt_to_income, :credit_score,
+      :when, :house_type, :budget, :where,
+      :inactive
+    )
+  end
+end
